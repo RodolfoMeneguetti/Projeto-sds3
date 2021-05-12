2c9583f260f9c0de8f2c854de81c3b6ee1102cd8
Paulo Rodolfo <paulo.meneguettitec@gmail.com>
2021-05-12 00:59:39 -0300

    NavBar // Footer

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-import './assets/css/styles.css';
+import './assets/css/styles.css'; /* ./ é o comando que importa pasta */ 
 import App from './App';
 
 
