508790084bc550bdd838a1ff694970c8206e297e
Rodolfogamer10 <paulo.meneguettitec@gmail.com>
2021-06-11 13:03:10 -0300

    Pagination

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,29 +1,38 @@
 import axios from "axios";
+import Pagination from "components/Pagination";
 import { useEffect, useState } from "react";
 import { SalePage } from "types/sale";
 import { formatLocalDate } from "utils/format";
 import { BASE_URL } from "utils/requests";
 
 const DataTable = () => {
-   
-   const [page, setPage] = useState<SalePage>({
-       first: true, 
-       last: true,
-       number: 0,
-       totalElements: 0,
-       totalPages: 0
-})
 
-useEffect(() => {
-    axios.get(`${BASE_URL}/sales?page=0&size=15&sort=date,desc`)
-    .then(response => {
-        setPage(response.data);
+    const [activePage, setActivePage] = useState(0);
+
+    const [page, setPage] = useState<SalePage>({
+        first: true,
+        last: true,
+        number: 0,
+        totalElements: 0,
+        totalPages: 0
     })
-}, []); 
 
-    return (
-       <div className="table-responsive">
-           <br></br> <table className="table table-striped table-sm">
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales?page=0&size=15&sort=date,desc`)
+            .then(response => {
+                setPage(response.data);
+            })
+    }, []);
+
+    const changePage = (index: number) => {
+    setActivePage(index);
+}
+
+return (
+    <>
+        <Pagination page={page} onPageChange = {changePage} />
+        <div className="table-responsive">
+            <br></br> <table className="table table-striped table-sm">
                 <thead>
                     <tr>
                         <th>Data</th>
@@ -34,22 +43,22 @@ useEffect(() => {
                     </tr>
                 </thead>
                 <tbody>
-                   {page.content?.map(item => (
-                    <tr key={item.id}>
-                        <td>{item.date}</td>
-                        <td>{item.seller.name}</td>
-                        <td>{item.visited}</td>
-                        <td>{item.deals}</td>
-                        <td>{item.amount.toFixed(2)}</td>
-                    </tr>
-                   ))}
-                   
-                    
+                    {page.content?.map(item => (
+                        <tr key={item.id}>
+                            <td>{item.date}</td>
+                            <td>{item.seller.name}</td>
+                            <td>{item.visited}</td>
+                            <td>{item.deals}</td>
+                            <td>{item.amount.toFixed(2)}</td>
+                        </tr>
+                    ))}
+
+
                 </tbody>
             </table>
         </div>
-
-    );
+    </>
+);
 }
 
 export default DataTable;
